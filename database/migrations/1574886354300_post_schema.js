e044a07a78451c9621f3fd04ae03215f3e578c7c
kielkow <matheuskiel@fiorifer.com.br>
2019-11-27 18:05:17 -0300

    Social media project

---
+++ database/migrations/1574886354300_post_schema.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class PostSchema extends Schema {
+  up () {
+    this.create('posts', (table) => {
+      table.increments()
+      table
+        .integer('user_id')
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.text('title')
+      table.text('content')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('posts')
+  }
+}
+
+module.exports = PostSchema
